825a462cbcb2fe3aeaaed2da7df4b13d29d1beb0
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-12-05 11:13:34 +0100

    Optionele user toegevoegd aan Post model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,9 +1,12 @@
+import { User } from './user';
+
 export class Post {
 
     constructor(
         public id: number,
         public userId: number,
         public title: string,
-        public body: string
+        public body: string,
+        public user?: User
     ) { }
 }
