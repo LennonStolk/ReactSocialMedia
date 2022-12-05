5a13a258b88e4185c3a5a58ebd736d45c3f6c0c4
Lennon <lennon27@live.nl>
2022-12-05 22:24:49 +0100

    Nieuwe "pages" directory gemaakt en Home pagina daar in gezet

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home'
+import Home from './pages/home/Home'
 import './index.css'
 import {
   createBrowserRouter,
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import './Home.css'
+import AppTitle from './components/AppTitle'
+
+function Home() {
+
+  return (
+    <div className='container'>
+      <main>
+        <AppTitle />
+      </main>
+    </div>
+  )
+}
+
+export default Home
