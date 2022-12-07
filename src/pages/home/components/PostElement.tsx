a17b7df7a6f39a02edd8cc88b8a2298ec2205b52
Lennon <lennon27@live.nl>
2022-12-07 22:08:31 +0100

    Nieuwe post hebben nu geen link naar niet  bestaande profiel pagina's meer

--- src/pages/home/components/PostElement.tsx
+++ src/pages/home/components/PostElement.tsx
@@ -8,6 +8,7 @@ function PostElement(props: { post: Post }) {
     const [liked, setLiked] = useState(false);
     const [likes, setLikes] = useState(Math.floor(Math.random() * 100));
 
+    // Rendert een ingedrukte of niet ingedrukte like knop
     function renderLikeButton() {
         if (liked)
             return <button className='post-like like-enabled' onClick={ () => toggleLiked() }> { likes } Likes 👍</button>
@@ -15,6 +16,7 @@ function PostElement(props: { post: Post }) {
             return <button className='post-like' onClick={ () => toggleLiked() }> { likes } Likes 👍</button>
     }
 
+    // Telt een like op, of trekt een like af
     function toggleLiked() {
         if (liked)
             setLikes(likes - 1)
@@ -24,10 +26,18 @@ function PostElement(props: { post: Post }) {
         setLiked(!liked);
     }
 
+    // Rendert linkt naar profiel als die bestaat
+    function renderProfileLink() {
+        if (props.post.userId == 0)
+            return <>{ props.post.user?.name }</>
+        else
+            return <Link to={ "/users/" + props.post.userId }>{ props.post.user?.name }</Link>
+    }
+
     return (
         <li className='post-element'>
             <h3 className='post-author'> 
-                <Link to={ "/users/" + props.post.userId }>{ props.post.user?.name }</Link>
+                { renderProfileLink() }
                 <span> van </span> 
                 { props.post.user?.company?.name } 
             </h3>
